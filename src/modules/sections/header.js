3c7577b9a892532f8fa8f55881f1f37baa95a5a4
kizzylion <kztchm@gmail.com>
2024-08-19 03:54:55 -0400

    FEAT: ADD RELOAD EVENT TO THE LOGO

--- src/modules/sections/header.js
+++ src/modules/sections/header.js
@@ -1,18 +1,18 @@
-import { getElementById } from "../utilities"
-import { themeSwitcher, setThemeIcon, fixThemeIcon } from "../themeSwitching"
+import { getElementById } from "../utilities";
+import { themeSwitcher, setThemeIcon, fixThemeIcon } from "../themeSwitching";
 
 export function addHeader(elem) {
-  let header = document.createElement("header")
+  let header = document.createElement("header");
   // header
-  header.innerHTML = headerUi()
-  header.id = "header"
-  header.classList.add("z-60")
-  elem.appendChild(header)
+  header.innerHTML = headerUi();
+  header.id = "header";
+  header.classList.add("z-60");
+  elem.appendChild(header);
 
-  const themeBtn = getElementById("themeSwitcher")
-  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
-
-  themeInitializer(prefersDarkScheme, themeBtn)
+  const themeBtn = getElementById("themeSwitcher");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  logoEvent();
+  themeInitializer(prefersDarkScheme, themeBtn);
 }
 
 function headerUi() {
@@ -24,16 +24,28 @@ function headerUi() {
                 <a  href="https://github.com/kizzylion" class="inline-block ml-auto mr:1 md:mr-2 text-2xl"><i class="bi bi-github"></i> </a>
                 <button id="themeSwitcher" class="px-4 py-2 text-2xl" ></button>
             </div>
-  `
+  `;
+}
+
+function logoEvent() {
+  // add event listener to logo. onclick, reload window
+  const logo = getElementById("logo");
+  logo.addEventListener("mouseover", (e) => {
+    //change cursor to pointer
+    logo.style.cursor = "pointer";
+  });
+  logo.addEventListener("click", (e) => {
+    window.location.reload();
+  });
 }
 
 function themeInitializer(preferredScheme, btn) {
-  setThemeIcon(btn)
+  setThemeIcon(btn);
 
   btn.addEventListener("click", function () {
-    themeSwitcher(btn)
-  })
+    themeSwitcher(btn);
+  });
   preferredScheme.onchange = function (event) {
-    fixThemeIcon(preferredScheme.matches, btn)
-  }
+    fixThemeIcon(preferredScheme.matches, btn);
+  };
 }
